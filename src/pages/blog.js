14c47c32ba0ca1deac3839d71f4b01f6f9f3d7f8
Gerard Mangues <gerardcoding@gmail.com>
2021-05-07 11:38:59 +0200

    Create a slug for every md file

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
 import Layout from '../components/layout';
 
-const Blog = () => (
-
-  <Layout>
-    <h2>Blog</h2>
-    <p>Posts are yet to be written!</p>
-  </Layout>
-);
+const Blog = () => {
+  const data = useStaticQuery(graphql`
+  query {
+    allMarkdownRemark{
+      edges{
+        node{
+          frontmatter{
+            title
+            date
+          }
+          html
+          excerpt
+        }
+      }
+    }
+  }
+  `);
+  return (
+    <Layout>
+      <ol>
+        {data.allMarkdownRemark.edges.map((post) => (
+          <li>
+            <h2>{post.node.frontmatter.title}</h2>
+            <p>{post.node.frontmatter.date}</p>
+          </li>
+        ))}
+      </ol>
+    </Layout>
+  );
+};
 
 export default Blog;
