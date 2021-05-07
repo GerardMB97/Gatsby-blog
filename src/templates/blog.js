d1c2c18ee48940ac0b6633ecc538098f8df276de
Gerard Mangues <gerardcoding@gmail.com>
2021-05-07 16:31:27 +0200

    Used renderRichText method to render the rich text field from the blog

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/no-danger */
 import React from 'react';
 import { graphql } from 'gatsby';
+import { renderRichText } from 'gatsby-source-contentful/rich-text';
 import Layout from '../components/layout';
 
 export const query = graphql`
@@ -10,6 +11,9 @@ query ($slug:String) {
   ) {
     title
     publishedDate(formatString: "MMMM Do, YYYY")
+    body {
+      raw
+    }
   
   }
 }
@@ -18,6 +22,11 @@ const Blog = ({ data }) => (
   <Layout>
     <h1>{data.contentfulBlogPost.title}</h1>
     <p>{data.contentfulBlogPost.publishedDate}</p>
+    <div>
+      {' '}
+      {renderRichText(data.contentfulBlogPost.body)}
+    </div>
+
   </Layout>
 );
 
