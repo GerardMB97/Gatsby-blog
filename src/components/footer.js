acbbf4be3ba1186b31969eed378b4caf33f17d6c
Gerard Mangues <gerardcoding@gmail.com>
2021-05-07 09:51:17 +0200

    Query author from siteMetadata

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
 
-const Footer = () => (
-  <footer>
-    <p>Created by Gerard Mangues ©2021</p>
-  </footer>
-);
+const Footer = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          author
+        }
+      }
+    }
+  `);
+
+  return (
+    <footer>
+      <p>
+        Created by
+        {' '}
+        {data.site.siteMetadata.author}
+        {' '}
+        ©2021
+      </p>
+    </footer>
+  );
+};
 
 export default Footer;
